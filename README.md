e0771fddd9bfbd38aea801f5719d62b8d0f9ecda
신승훈 <zlzlzlmo@daum.net>
2022-09-25 20:57:54 +0900

    style: box shadow

---
+++ README.md
@@ -0,0 +1,5 @@
+## What I learned
+
+- React Native에서 제공되는 Button 컴포넌트는 기본적으로 Touchable, View, Text 컴포넌트들의 조합으로 이루어져있다. (https://github.com/facebook/react-native/blob/main/Libraries/Components/Button.js)
+
+- React Native에는 웹브라우저에서 사용되는 boxShadow기능이 없으며, 안드로이드는 elevation을 사용하며, IOS는 shadowColor, shadowOffset : {width, height},shadowRadius, shadowOpacity를 사용한다.
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -14,4 +14,17 @@ const StartGameScreen = () => {
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
