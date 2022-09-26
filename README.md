d7f3e15c2548ccced03fe85d90f9404b971efdbe
신승훈 <zlzlzlmo@daum.net>
2022-09-26 21:00:33 +0900

    feat: primary button

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 - React Native에는 웹브라우저에서 사용되는 boxShadow기능이 없으며, 안드로이드는 elevation을 사용하며, IOS는 shadowColor, shadowOffset : {width, height},shadowRadius, shadowOpacity를 사용한다.
 
 - TextInput API에서 keyboardType 으로 숫자만 입력 가능하게 할것인지 등 설정 할 수 있다. (number-pad 이면 숫자만, 그 외 여러가지 속성값들이 있다), 추가적으로 autoCorrect 및 autoCapitalize 등 자동으로 무언가 변할때 사용되는 속성을 컨트롤 하여서 사용자 경험을 저해시키는 요소들을 제거 할 수도 있다.
+
+- 클릭이 가능한 컨테이너를 만들때 Pressable API를 이용할 수 있다.
+  - TouchableOpactiy라는 것도 있지만, Pressable은 onPressIn, Out, LongPress등 다양한 상황에 대해 이벤트를 줄 수 있따.
+  - android-ripple 속성으로 클릭했을때 효과도 줄 수 있다 (하지만 이것은 안드로이드만 가능)
+  - android-ripple에서 의도한대로 파급효과를 주기 위해선 View 컴포넌트 안에 Pressable을 넣고 그 안에 속성값을 줘야한다. 즉 View -> Pressable -> Text
+  - style 속성안에 pressed 프로퍼티가 내부적으로 들어오는데 이 boolean값으로 눌렀을때의 스타일링을 할 수 있다.
--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 
 interface Props {
@@ -6,13 +6,43 @@ interface Props {
 }
 
 const PrimaryButton: React.FC<Props> = ({ children }) => {
+  const onClick = () => console.log("clicked");
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={onClick}
+        android_ripple={{ color: "#c52a78" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    overflow: "hidden",
+    margin: 4,
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#a80858",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
