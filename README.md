98c4d008bb3e27abc427ec6337f26d072faff776
신승훈 <zlzlzlmo@daum.net>
2022-09-26 20:34:46 +0900

    feat: text input

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - React Native에서 제공되는 Button 컴포넌트는 기본적으로 Touchable, View, Text 컴포넌트들의 조합으로 이루어져있다. (https://github.com/facebook/react-native/blob/main/Libraries/Components/Button.js)
 
 - React Native에는 웹브라우저에서 사용되는 boxShadow기능이 없으며, 안드로이드는 elevation을 사용하며, IOS는 shadowColor, shadowOffset : {width, height},shadowRadius, shadowOpacity를 사용한다.
+
+- TextInput API에서 keyboardType 으로 숫자만 입력 가능하게 할것인지 등 설정 할 수 있다. (number-pad 이면 숫자만, 그 외 여러가지 속성값들이 있다), 추가적으로 autoCorrect 및 autoCapitalize 등 자동으로 무언가 변할때 사용되는 속성을 컨트롤 하여서 사용자 경험을 저해시키는 요소들을 제거 할 수도 있다.
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -5,7 +5,13 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -16,7 +22,6 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     marginTop: 100,
     padding: 16,
     backgroundColor: "#72063c",
@@ -26,5 +31,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+    marginHorizontal: 20,
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
